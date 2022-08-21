154d0e2d3422df298568a313318a4c04bb26e78c
dhwlxn <109324394+dhwlxn@users.noreply.github.com>
2022-08-22 02:29:21 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 156 ms, Memory: 61,452 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,28 +1,28 @@
+#0으로 감싸기
+def puzzle(arr2, n, k):
+    cnt_lst = []
+    ans = 0
+    for i in range(n+2):
+        cnt = 0
+        for j in range(n+2):
+            if arr2[i][j]:
+                cnt += 1
+            else:
+                if cnt != 0:
+                    cnt_lst.append(cnt)
+                cnt = 0
+
+    for c in cnt_lst:
+        if c == k:
+            ans += 1
+    return ans
+
+
 tc = int(input())
 for t in range(1, tc+1):
-    n, k = map(int, input().split())
-    lst = []
-    zip_lst = []
-    col_lst = []
-    #row 단위로 input 받는데, 공백을 제거하고 0으로 split
-    #=> 1만 남겨서 k와 길이비교
-    for _ in range(n):
-        arr = ''.join(input().split())
-        lst.append(arr.split('0'))
-        zip_lst.append(arr)
-        cnt_r = 0
-        for row in lst:
-            for r in row:
-                if len(r) == k:
-                    cnt_r += 1
-    #한 개의 테케 안의 arr를 모은 lst_2를 zip 하여 세로 단어 구함.
-    for z in zip(*zip_lst):
-        z = ''.join(z)
-        z_lst = z.split('0')
-        col_lst.append(z_lst)
-        cnt_c = 0
-        for col in col_lst:
-            for c in col:
-                if len(c) == k:
-                    cnt_c += 1
-    print(f'#{t} {cnt_r + cnt_c}')
+    N, K = map(int, input().split())
+    lst = [[0] + list(map(int, input().split())) + [0] for _ in range(N)]
+    lst2 = [[0] * (N+2)] + lst + [[0] * (N+2)]
+    z_arr = list(zip(*lst2))
+    print(f'#{t} {puzzle(lst2, N, K) + puzzle(z_arr, N, K)}')
+
