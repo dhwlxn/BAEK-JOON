8a3a55424ba5a52bdd19c676d8cda21d65d5203d
dhwlxn <109324394+dhwlxn@users.noreply.github.com>
2022-09-04 18:43:11 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,16 @@
+alpha = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z'
+]
+
+location = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+]
+
+s = list(input())
+for i in range(len(alpha)):
+    if alpha[i] in s:
+        location[i] = s.index(alpha[i])
+print(*location, end=' ')
